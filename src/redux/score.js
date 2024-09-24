66e816b25953b1cb49c74ac393b3278edbf54145
KilonzoJames <jay.kilonzo@gmail.com>
2024-09-24 16:59:02 +0300

    Added more states to redux

--- src/redux/score.js
+++ src/redux/score.js
@@ -1,7 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
   value: 0,
+  showDialog: false,
+  musicPlaying: false,
+  isSnakeMoving: true,
 };
+
 const scoreSlice = createSlice({
   name: "score",
   initialState,
@@ -12,7 +17,18 @@ const scoreSlice = createSlice({
     decrement: (state) => {
       state.value = 0; // Reset to zero
     },
+    setShowDialog: (state) => {
+      state.showDialog = !state.showDialog; // Correctly toggle the showDialog property
+    },
+    setMusicPlaying: (state) => {
+      state.musicPlaying = !state.musicPlaying; // Correctly toggle the musicPlaying property
+    },
+    setIsSnakeMoving: (state) => {
+      state.isSnakeMoving = !state.isSnakeMoving; // Correctly toggle the isSnakeMoving property
+    },
   },
 });
-export const { increment, decrement } = scoreSlice.actions;
+
+export const { increment, decrement, setShowDialog, setMusicPlaying, setIsSnakeMoving } =
+  scoreSlice.actions;
 export default scoreSlice.reducer;
